86e54206ff87887ce11e1a18c4e50daff6849ae3
ThibautLemmens <thibautlemmens@gmail.com>
2025-02-04 23:31:48 +0000

    u_Sprite fix

--- src/KomOS/u_Sprite.lua
+++ src/KomOS/u_Sprite.lua
@@ -15,21 +15,21 @@
 --Function
 local SpriteRender = {}
 
-function SpriteRender:DrawImage(x,y,image)
+function SpriteRender.DrawImage(x,y,image)
 	for i = 0, #image-1 do
 		term.setCursorPos(x ,y+i)
 		term.write(image[i+1])
 	end
 end
 
-function SpriteRender:DrawImageMonitor(x,y,monitor,image)
+function SpriteRender.DrawImageMonitor(x,y,monitor,image)
 	for i = 0, #image-1 do
 		monitor.setCursorPos(x,y+i)
 		monitor.write(image[i+1])
 	end
 end
 
-function SpriteRender:GetSize(image)
+function SpriteRender.GetSize(image)
 	local x,y=0
 	str = image[1]
 	x = #str
