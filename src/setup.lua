dab8aedd4c3d90f3965d154d09d58b438430b1cf
Jari Wilms <jariwilms@outlook.com>
2025-02-05 17:16:53 +0100

    basic installer

--- src/setup.lua
+++ src/setup.lua
@@ -9,8 +9,11 @@ function main()
     }
 
     for _, value in ipairs(directories) do
-        fs.makeDir(value)
+        if not fs.exists(value) then fs.makeDir(value) end
     end
+
+    shell.run("wget", [[https://raw.githubusercontent.com/jariwilms/ComputerCraft/refs/heads/main/src/bin/git_clone.lua]], "bin/git_clone")
+    shell.run("wget", [[https://raw.githubusercontent.com/jariwilms/ComputerCraft/refs/heads/main/src/cfg/git_clone.lua]], "cfg/git_clone")
 end
 
 main()
