14055de7a743d0139bca2bf364ec9034e10c9f39
Jari Wilms <jariwilms@outlook.com>
2025-02-06 20:45:15 +0100

    meta library

---
+++ src/lib/meta.lua
@@ -0,0 +1,16 @@
+local meta =
+{
+    read_only = function(_)
+        local proxy = {}
+        local meta =
+        {
+            __index    = _,
+            __newindex = function (_,__,___) error("attempt to update a read-only table", 2) end
+        }
+
+        setmetatable(proxy, meta)
+        return proxy
+    end
+}
+
+return meta
