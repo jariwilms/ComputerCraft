66c3437799930f6143a45f0566ac8d80674ae47a
Jari Wilms <jariwilms@outlook.com>
2025-02-06 19:02:26 +0100

    force reinstall and refactor

--- src/setup.lua
+++ src/setup.lua
@@ -1,19 +1,29 @@
 local function main()
-    local directories = 
+    local directories =
     {
-        "/bin", 
-        "/cfg", 
-        "/dev", 
-        "/lib", 
-        "/log", 
+        "/bin",
+        "/cfg",
+        "/dev",
+        "/lib",
+        "/log",
+    }
+    local programs =
+    {
+        git_clone_bin = { path = "bin/git_clone", url = [[https://raw.githubusercontent.com/jariwilms/ComputerCraft/refs/heads/main/src/bin/git_clone.lua]] },
+        git_clone_cfg = { path = "cfg/git_clone", url = [[https://raw.githubusercontent.com/jariwilms/ComputerCraft/refs/heads/main/src/cfg/git_clone.lua]] },
     }
 
     for _, value in ipairs(directories) do
         if not fs.exists(value) then fs.makeDir(value) end
     end
 
-    shell.run("wget", [[https://raw.githubusercontent.com/jariwilms/ComputerCraft/refs/heads/main/src/bin/git_clone.lua]], "bin/git_clone")
-    shell.run("wget", [[https://raw.githubusercontent.com/jariwilms/ComputerCraft/refs/heads/main/src/cfg/git_clone.lua]], "cfg/git_clone")
+    for _, value in pairs(programs) do
+        local path = value.path
+        local url  = value.url
+
+        if fs.exists(path) then fs.delete(path) end
+        shell.run("wget", url, path)
+    end
 end
 
 main()
