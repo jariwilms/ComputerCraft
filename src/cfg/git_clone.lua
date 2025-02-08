7deea1625376ba56074add2f20db768c66892c16
Jari Wilms <jariwilms@outlook.com>
2025-02-08 23:20:31 +0100

    removed meta dependency

--- src/cfg/git_clone.lua
+++ src/cfg/git_clone.lua
@@ -9,7 +9,6 @@ local urls =
             dependencies =
             {
                 { url = "src/lib/item.lua", path = "lib/item" },
-                { url = "src/lib/meta.lua", path = "lib/meta" },
             },
         },
         {
