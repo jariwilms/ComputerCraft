71d633120516d9a4e697c9268fdbf4a1bb7964a4
Jari Wilms <jariwilms@outlook.com>
2025-02-08 16:35:57 +0100

    dependency update

--- src/cfg/git_clone.lua
+++ src/cfg/git_clone.lua
@@ -5,13 +5,21 @@ local urls =
     required =
     {
         {
-            url = "src/bin/mine_area.lua", path = "bin/mine_area",
+            url = "src/lib/inventory.lua", path = "/lib/inventory",
             dependencies =
             {
-                { url = "src/cfg/mine_area.lua", path = "cfg/mine_area" },
-                { url = "src/lib/meta.lua",      path = "lib/meta" },
-                { url = "src/lib/math_ext.lua",  path = "lib/math_ext" },
-                { url = "src/lib/terra.lua",     path = "lib/terra" },
+                --{ url = "src/cfg/inventory.lua", path = "cfg/inventory" },
+                { url = "src/lib/item.lua", path = "/lib/item" },
+            },
+        },
+        {
+            url = "src/bin/mine_area.lua", path = "/bin/mine_area",
+            dependencies =
+            {
+                { url = "src/cfg/mine_area.lua", path = "/cfg/mine_area" },
+                { url = "src/lib/meta.lua",      path = "/lib/meta" },
+                { url = "src/lib/math_ext.lua",  path = "/lib/math_ext" },
+                { url = "src/lib/terra.lua",     path = "/lib/terra" },
             },
         },
     },
