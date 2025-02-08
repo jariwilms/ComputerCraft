8c6747c06dfc3632168021b8da3e2e011ad7156c
Jari Wilms <jariwilms@outlook.com>
2025-02-08 16:19:50 +0100

    multiple bugfixes

--- src/lib/inventory.lua
+++ src/lib/inventory.lua
@@ -14,6 +14,13 @@ local item = {}
 function item.new(identifier, count, limit, damage)
     return setmetatable({ identifier = identifier, count = count, limit = limit, damage = damage or 0 },
     {
+        __eq = function (left, right)
+            return
+                left.identifier == right.identifier --and
+                --left.count      == right.count      and
+                --left.limit      == right.limit      and
+                --left.damage     == right.damage
+        end,
         __tostring = function (self) return self.identifier .. ", [" .. self.count .. "/" .. self.limit .. "], " .. self.damage end
     })
 end
@@ -31,21 +38,13 @@ function item.empty()
     return item.new("None", 0, 0, 0)
 end
 
----Compares items by identifier
----@param  left  item
----@param  right item
----@return       boolean
-function item.__eq(left, right)
-    return
-        left.identifier == right.identifier --and
-        --left.count      == right.count      and
-        --left.limit      == right.limit      and
-        --left.damage     == right.damage
-end
 
 
 
 
+---Represents the internal state of a turtle's inventory
+---Indices are valid in the range [1, 16]
+---Methods will always restore the previously selected index
 ---@class inventory
 local inventory =
 {
@@ -67,18 +66,19 @@ local inventory =
     [16] = item.empty(),
 }
 
-function inventory.check_bounds(index)
-    if index < 1 or index > 16 then error("Index out of range!", 2) end
+---Returns true if the given index is valid
+---@param index integer
+function inventory.in_bounds(index)
+    return index >= 1 and index <= 16
 end
 
 ---Updates the inventory slot at index or selected if no index is given
----@param index? integer
+---@param index integer
 function inventory.update(index)
-    index = index or turtle.getSelectedSlot()
-    inventory.check_bounds(index)
+    if not inventory.in_bounds(index) then error("Index out of range!", 2) end
 
     local data = turtle.getItemDetail(index)
-    if data then inventory[index] = item.new(data.name, data.count, turtle.getItemSpace(index), data.damage) end
+    if data then inventory[index] = item.new(data.name, data.count, data.count + turtle.getItemSpace(index), data.damage) end
 end
 
 ---Updates all inventory slots
@@ -92,7 +92,7 @@ end
 ---@param  index integer
 ---@return       item
 function inventory.at(index)
-    inventory.check_bounds(index)
+    if not inventory.in_bounds(index) then error("Index out of range!", 2) end
 
     return item.copy(inventory[index])
 end
@@ -127,9 +127,14 @@ end
 ---@param  amount? integer
 ---@return         boolean
 function inventory.transfer(from, to, amount)
-    if from ~= to and turtle.select(from) and turtle.transferTo(to, amount) then
-        inventory[to]   = inventory[from]
-        inventory[from] = item.empty()
+    if not inventory.in_bounds(from) then error("Index out of range!", 2) end
+    if not inventory.in_bounds(to)   then error("Index out of range!", 2) end
+
+    if from == to then return false end
+
+    if turtle.select(from) and turtle.transferTo(to, amount) then
+        inventory.update(from)
+        inventory.update(to)
 
         return true
     end
@@ -142,15 +147,18 @@ end
 ---@param  right integer
 ---@return       boolean
 function inventory.swap(left, right)
+    if not inventory.in_bounds(left)  then error("Index out of range!", 2) end
+    if not inventory.in_bounds(right) then error("Index out of range!", 2) end
+
     if left == right    then return false end
     if inventory.full() then return false end
 
     local placeholder = inventory.find(item.empty().identifier)
     if not placeholder then return false end
 
-    inventory.transfer(left,        placeholder)
-    inventory.transfer(right,       left)
-    inventory.transfer(placeholder, right)
+    if not inventory.transfer(left,        placeholder) then return false end
+    if not inventory.transfer(right,       left)        then return false end
+    if not inventory.transfer(placeholder, right)       then return false end
 
     return true
 end
@@ -159,7 +167,7 @@ end
 ---@param index   integer
 ---@param amount? integer
 function inventory.drop(index, amount)
-    inventory.check_bounds(index)
+    if not inventory.in_bounds(index) then error("Index out of range!", 2) end
 
     if amount and amount > 0 then amount = math.min(amount, inventory[index].count)
     else                          amount = inventory[index].count
@@ -175,7 +183,7 @@ end
 function inventory.drop_all()
     for index, _ in ipairs(inventory) do
         turtle.select(index)
-        turtle.drop(index)
+        turtle.drop()
     end
 
     inventory.update_all()
@@ -243,12 +251,13 @@ function inventory.defragment(offset)
 end
 
 
+
 setmetatable(inventory,
 {
     __tostring = function(self)
         local str = ""
-        for _, value in ipairs(self) do
-            str = str .. tostring(value) .. "\n"
+        for index, value in ipairs(self) do
+            str = str .. "[" .. index .. "] ".. tostring(value) .. "\n"
         end
 
         return str
@@ -257,6 +266,5 @@ setmetatable(inventory,
 
 
 
-
-
+return inventory, item
 
