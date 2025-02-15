f7971a18331134f22fa321f2344cbd6dbdd0b183
ThibautLemmens <thibautlemmens@gmail.com>
2025-02-15 00:59:00 +0000

    update

--- src/KomOS/Debug_Test.lua
+++ src/KomOS/Debug_Test.lua
@@ -1,11 +1,61 @@
-local chests = {peripheral.find("minecraft:chest")}
+--Chest Program
 
-print(tostring(#chests))
+--Includes
+local ChestAPI = require("u_Chest")
+local Log = require("u_Log")
 
-for i, chest in ipairs(chests) do
-    print(peripheral.getType(chest))
+--Config
+Log.Init("l_Chest",true)
+
+--Global
+local chests = {peripheral.find("Inventory")}
+local item = {name = "", count = 0}
+local TotalList
+
+--Data
+
+--Class
+
+--Functionw2
+function ScanAllChest()
+    local ChestAPITable
+    table.clear(item)
+    for i = 1, #chests do
+        table.insert(ChestAPITable, i, ChestAPI)
+        ChestAPITable[i].Init(chests[i])
+        local chestItems = ChestAPITable[i].GetSimpleInv()
+        for j=1, #chestItems do
+            AddToTotalList(chestItems[i]["name"], chestItems[i]["count"])
+        end
+    end
 end
 
-local modem = peripheral.find("modem")
-local networkchests = modem.hasTypeRemote("", "minecraft:chest")
-print(textutils.serialise(networkchests))
+function AddToTotalList(name, count)
+    for index, value in ipairs(TotalList) do
+        if value["name"] == name then
+            value.count = value.count + count
+            return true
+        end
+    end
+    TotalList[name] = count
+    return true
+end
+
+function PrintTotalList()
+    for index, value in ipairs(TotalList) do
+       print(value.name..":\t\t"..value.count)
+       sleep(0.2)
+    end
+end
+
+--Main
+ScanAllChest()
+PrintTotalList()
+
+function Stop()
+    local event,key = os.pullEvent("key")
+    return
+end
+
+--print("farming...\n <press any key to stop>")
+--parallel.waitForAny(RunFarm, Stop)
