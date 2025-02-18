33763e9f2dd7f8c50e79cef20199bf6dee6b2cb5
Jari Wilms <jariwilms@outlook.com>
2025-02-18 07:58:39 +0100

    default burn time

--- src/cfg/fuel.lua
+++ src/cfg/fuel.lua
@@ -1,10 +1,12 @@
 local config =
 {
     ["minecraft:lava_bucket"]  = 1000,
-    ["minecraft:blaze_rod"]    = 120,
-    ["minecraft:coal"]         = 80,
-    ["minecraft:charcoal"]     = 80,
-    ["minecraft:coal_block"]   = 80,
+    ["minecraft:blaze_rod"]    =  120,
+    ["minecraft:coal"]         =   80,
+    ["minecraft:charcoal"]     =   80,
+    ["minecraft:coal_block"]   =   80,
+
+    DefaultBurnTime            =   10,
 }
 
 return config
